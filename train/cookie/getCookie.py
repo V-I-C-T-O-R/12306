8ce07ea28d2eda88ca8af7bc2ccc5a2cadabe4c0
zhouminghua <zhoumh1988@163.com>
2019-11-29 14:30:42 +0800

    1、获取cookie优化

--- train/cookie/getCookie.py
+++ train/cookie/getCookie.py
@@ -1,5 +1,6 @@
 import random
 import os
+import time
 from selenium import webdriver
 from net import ips
 
@@ -7,17 +8,23 @@ def get12306Cookie():
     proxy = random.choice(ips)
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.add_argument("'--proxy-server={}".format(proxy))
-    browser = webdriver.Chrome(chrome_options = chromeOptions, executable_path=os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/cookie/chromedriver')
+    browser = webdriver.Chrome(chrome_options=chromeOptions, executable_path=os.path.dirname(
+        os.path.dirname(os.path.realpath(__file__))) + '/cookie/chromedriver')
     browser.get("https://www.12306.cn/index/")
-    cookies = browser.get_cookies()
+    flag = False
     res = {}
-    for cookie in cookies:
-        if cookie['name'] == 'RAIL_DEVICEID':
-            res['RAIL_DEVICEID'] = cookie['value']
-        elif cookie['name'] == 'RAIL_EXPIRATION':
-            res['RAIL_EXPIRATION'] = cookie['value']
+    while (flag == False):
+        cookies = browser.get_cookies()
+        for cookie in cookies:
+            if cookie['name'] == 'RAIL_DEVICEID':
+                res['RAIL_DEVICEID'] = cookie['value']
+                flag = True
+            elif cookie['name'] == 'RAIL_EXPIRATION':
+                res['RAIL_EXPIRATION'] = cookie['value']
+        time.sleep(1)
     browser.quit()
     return res
+
 
 if __name__ == '__main__':
     cookies = get12306Cookie()
